830ab36bc47ed4c547f64c4b17780eb5ed48e730
CouchCamper <Johannestraupe@gmx.de>
2020-08-23 02:50:02 +0200

    großes parabelförmiges Tragseil
    not working

--- Main.py
+++ Main.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 # Punkt Q(x|y)
 qX = 812
 qY = 177
@@ -14,8 +16,29 @@ g = 0
 for i in range(1, 34):
     x = qX - (i * d)
     y = a * pow(x, 2)
-    print("Punkt " + str(i) + " (" + str(x) + " | " + str(y) +")")
-    g = g + (2 * y)
+    #print("Punkt " + str(i) + " (" + str(x) + " | " + str(y) + ")")
+    g = g + (4 * y)
 
 print("\nFunktionsgleichung: f(x) = " + str(a) + " * x^2")
-print("\nDie Gesamtlänge der senkrechten Tragseile beträgt " + str(g) + " Meter.")
+print("\nDie Gesamtlänge der senkrechten Tragseile beträgt " + str(g) + " Meter.\n")
+
+
+# Steigungsdreieck; je kleiner deltaX, desto genauer die Länge des Seils
+deltaX = qX / 2
+deltaY = 0
+
+preY = qY
+
+gesC = 0
+
+for j in range(1, qX // 2):
+    x = qX - j
+    y = a * pow(x, 2)
+    print("Punkt " + str(j) + " (" + str(x) + " | " + str(y) + ")")
+    deltaY = preY - y
+    c = sqrt(pow(deltaX, 2) + pow(deltaY, 2))
+    gesC += c
+    print(str(c))
+
+print("\nDie Gesamtlänge des großen parabelförmigen Tragseils beträgt " + str(gesC) + " Meter.")
+print(str(2 * sqrt(pow(qX, 2) + pow(qY, 2))))
